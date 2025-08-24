5a0936442833b48edf222b436378ab56604bac6e
Babajide789 <yahayababajide@gmail.com>
2025-08-24 19:58:52 -0400

    cleaned the layout.tsx for unused functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,7 @@ import type { Metadata } from 'next';
 import Footer from './components/footer'
 
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 
-
-const theme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#1976d2",
-    },
-  },
-});
 
 export const metadata: Metadata = {
   title: 'Canvas Lancer',
